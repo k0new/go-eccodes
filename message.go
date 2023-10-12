e5964b05b822a18f696e7d715a5df6a1e050398c
ivankonevv <ivankonewvv@yandex.by>
2023-10-12 18:27:08 +0400

    go mod changed to github.com/k0new/go-eccodes

--- message.go
+++ message.go
@@ -12,17 +12,34 @@ type Message interface {
 	isOpen() bool
 
 	GetString(key string) (string, error)
+	GetStringArray(key string) ([]string, error)
 
 	GetLong(key string) (int64, error)
 	SetLong(key string, value int64) error
 
 	GetDouble(key string) (float64, error)
 	SetDouble(key string, value float64) error
+	GetDoubleArray(key string) ([]float64, error)
 
 	Data() (latitudes []float64, longitudes []float64, values []float64, err error)
 	DataUnsafe() (latitudes *Float64ArrayUnsafe, longitudes *Float64ArrayUnsafe, values *Float64ArrayUnsafe, err error)
 
 	Close() error
+
+	KeyIterator() Iterator
+
+	Clone() (Message, error)
+}
+
+type Iterator interface {
+	isOpen() bool
+	Next() bool
+	GetName() string
+	Close() error
+}
+
+type iterator struct {
+	handle native.Ccodes_handle
 }
 
 type message struct {
@@ -39,12 +56,49 @@ func newMessage(h native.Ccodes_handle) Message {
 	return m
 }
 
+func newIterator(h native.Ccodes_handle) Iterator {
+	i := &iterator{handle: h}
+	runtime.SetFinalizer(i, iteratorFinalizer)
+	return i
+}
+
+func (i *iterator) GetName() string {
+	return native.Ccodes_keys_iterator_get_name(i.handle)
+}
+
+func (i *iterator) Next() bool {
+	return native.Ccodes_keys_iterator_next(i.handle) == 1
+}
+
+func (i *iterator) isOpen() bool {
+	return i.handle != nil
+}
+
+func (i *iterator) Close() error {
+	defer func() { i.handle = nil }()
+	return native.Ccodes_keys_iterator_delete(i.handle)
+}
+
 func (m *message) isOpen() bool {
 	return m.handle != nil
 }
 
+func (m *message) Clone() (Message, error) {
+	single, err := native.Ccodes_clone(m.handle)
+	if err != nil {
+		return nil, err
+	}
+	return newMessage(single), nil
+}
+
 func (m *message) GetString(key string) (string, error) {
 	return native.Ccodes_get_string(m.handle, key)
+}
+
+func (m *message) KeyIterator() Iterator {
+	iter := native.Ccodes_keys_iterator_new(m.handle, 0, "")
+
+	return newIterator(iter)
 }
 
 func (m *message) GetLong(key string) (int64, error) {
@@ -57,6 +111,13 @@ func (m *message) SetLong(key string, value int64) error {
 
 func (m *message) GetDouble(key string) (float64, error) {
 	return native.Ccodes_get_double(m.handle, key)
+}
+
+func (m *message) GetStringArray(key string) ([]string, error) {
+	return native.Ccodes_get_string_array(m.handle, key)
+}
+func (m *message) GetDoubleArray(key string) ([]float64, error) {
+	return native.Ccodes_get_double_array(m.handle, key)
 }
 
 func (m *message) SetDouble(key string, value float64) error {
@@ -84,5 +145,12 @@ func messageFinalizer(m *message) {
 	if m.isOpen() {
 		debug.MemoryLeakLogger.Print("message is not closed")
 		m.Close()
+	}
+}
+
+func iteratorFinalizer(i *iterator) {
+	if i.isOpen() {
+		debug.MemoryLeakLogger.Print("iterator is not closed")
+		i.Close()
 	}
 }
--- native/eccodes-values.go
+++ native/eccodes-values.go
@@ -5,6 +5,8 @@ package native
 */
 import "C"
 import (
+	"io"
+	"reflect"
 	"unsafe"
 
 	"github.com/pkg/errors"
@@ -27,6 +29,18 @@ func Ccodes_get_long(handle Ccodes_handle, key string) (int64, error) {
 	}
 
 	return int64(value), nil
+}
+
+func Ccodes_clone(handle Ccodes_handle) (Ccodes_handle, error) {
+	h, err := C.codes_handle_clone((*C.codes_handle)(handle))
+	//if err != 0 {
+	//	return nil, errors.New(Cgrib_get_error_message(int(err)))
+	//}
+	if h == nil {
+		return nil, io.EOF
+	}
+
+	return unsafe.Pointer(h), err
 }
 
 func Ccodes_set_long(handle Ccodes_handle, key string, value int64) error {
@@ -65,6 +79,140 @@ func Ccodes_set_double(handle Ccodes_handle, key string, value float64) error {
 	}
 
 	return nil
+}
+
+func freeStringArray(strArray **C.char, len C.int) {
+	if strArray == nil {
+		return
+	}
+
+	sliceHeader := reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(strArray)),
+		Len:  int(len),
+		Cap:  int(len),
+	}
+	slice := *(*[]*C.char)(unsafe.Pointer(&sliceHeader))
+
+	for i := range slice {
+		C.free(unsafe.Pointer(slice[i]))
+	}
+
+	C.free(unsafe.Pointer(strArray))
+}
+
+func Ccodes_get_string_array(handle Ccodes_handle, key string) ([]string, error) {
+	cKey := C.CString(key)
+	defer C.free(unsafe.Pointer(cKey))
+	var err C.int
+	var size int
+	cSize := (*C.size_t)(unsafe.Pointer(&size))
+
+	//var strArr []string
+	//cArr := (**C.char)(unsafe.Pointer(&strArr[0]))
+
+	err = C.codes_get_size((*C.codes_handle)(handle), cKey, cSize)
+	if err != 0 {
+		return nil, errors.New(Cgrib_get_error_message(int(err)))
+	}
+	strArray := (**C.char)(C.malloc(C.size_t(*cSize) * C.size_t(unsafe.Sizeof((*C.char)(nil)))))
+	if strArray == nil {
+		return nil, errors.New("Error: Could not allocate memory")
+	}
+
+	err = C.codes_get_string_array((*C.codes_handle)(handle), cKey, strArray, cSize)
+	if err != 0 {
+		return nil, errors.New(Cgrib_get_error_message(int(err)))
+	}
+
+	//length := CsizeT(MaxStringLength)
+	//cLength := (*C.size_t)(unsafe.Pointer(&length))
+	//
+	//err := C.codes_get_length((*C.codes_handle)(handle), cKey, cLength)
+	//if err != 0 {
+	//	return nil, errors.New(Cgrib_get_error_message(int(err)))
+	//}
+	//// +1 byte for '\0'
+	//length++
+	//
+	//var cBytes *C.char
+	//var result []byte
+	//
+	//if length > MaxStringLength {
+	//	debug.MemoryLeakLogger.Printf("unnecessary memory allocation - length of '%s' value is %d greater than MaxStringLength=%d",
+	//		key, int(length), MaxStringLength)
+	//	result = make([]byte, length)
+	//} else {
+	//	var buffer [MaxStringLength]byte
+	//	result = buffer[:]
+	//}
+	//
+	//cBytes = (*C.char)(unsafe.Pointer(&result[0]))
+	//err = C.codes_get_string_array((*C.codes_handle)(handle), cKey, cBytes, cLength)
+	//if err != 0 {
+	//	return "", errors.New(Cgrib_get_error_message(int(err)))
+	//}
+	//
+	//if length == 0 {
+	//	return "", nil
+	//}
+	//return string(result[:length-1]), nil\
+	goStrings := make([]string, int(*cSize))
+	charSize := int(unsafe.Sizeof((*C.char)(nil)))
+	for i := 0; i < len(goStrings); i++ {
+		str := C.GoString((*C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(*strArray)) + uintptr(i)*uintptr(charSize))))
+		goStrings[i] = str
+	}
+
+	// Free allocated memory for the string array
+	C.free(unsafe.Pointer(strArray))
+	return goStrings, nil
+	//// +1 byte for '\0'
+	//length++
+	//
+	//var cBytes *C.char
+	//var result []byte
+	//
+	//if length > MaxStringLength {
+	//	debug.MemoryLeakLogger.Printf("unnecessary memory allocation - length of '%s' value is %d greater than MaxStringLength=%d",
+	//		key, int(length), MaxStringLength)
+	//	result = make([]byte, length)
+	//} else {
+	//	var buffer [MaxStringLength]byte
+	//	result = buffer[:]
+	//}
+	//
+	//cBytes = (*C.char)(unsafe.Pointer(&result[0]))
+	//err = C.codes_get_string((*C.codes_handle)(handle), cKey, cBytes, cLength)
+	//if err != 0 {
+	//	return "", errors.New(Cgrib_get_error_message(int(err)))
+	//}
+	//
+	//if length == 0 {
+	//	return "", nil
+	//}
+	//return string(result[:length-1]), nil
+}
+
+func Ccodes_get_double_array(handle Ccodes_handle, key string) ([]float64, error) {
+	cKey := C.CString(key)
+	defer C.free(unsafe.Pointer(cKey))
+	var err C.int
+	var size int
+	cSize := (*C.size_t)(unsafe.Pointer(&size))
+
+	err = C.codes_get_size((*C.codes_handle)(handle), cKey, cSize)
+	if err != 0 {
+		return nil, errors.New(Cgrib_get_error_message(int(err)))
+	}
+	res := make([]float64, size)
+	cRes := (*C.double)(unsafe.Pointer(&res[0]))
+
+	err = C.codes_get_double_array((*C.codes_handle)(handle), cKey, cRes, cSize)
+	if err != 0 {
+		return nil, errors.New(Cgrib_get_error_message(int(err)))
+	}
+
+	return res, nil
 }
 
 func Ccodes_get_string(handle Ccodes_handle, key string) (string, error) {
